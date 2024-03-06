9c21f54de025c25d7ca553fb7f676ffd289ef833
awmtea <awmtea105.infinity@gmail.com>
2024-03-06 16:03:40 +0700

    palindrome

---
+++ palindrome/src/main.rs
@@ -0,0 +1,23 @@
+fn palindrome<T: PartialEq>(input: &[T]) -> bool {
+    let len = input.len();
+
+    for i in 0..len / 2 {
+        if input[i] != input[len - i - 1] {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    let a = "racecar";
+    let b = [1,2,3,3,4,23];
+    let c = String::from("hello world");
+    let d = vec![6,7,8,9,0,9,8,7,6];
+
+    println!("{} is palindrome: {:?}", a, palindrome(&a.chars().collect::<Vec<_>>()));
+    println!("{:?} is palindrome: {:?}", b, palindrome(&b));
+    println!("{:?} is palindrome: {:?}", c, palindrome(&c.chars().collect::<Vec<_>>()));
+    println!("{:?} is palindrome: {:?}", d, palindrome(&d));
+}
